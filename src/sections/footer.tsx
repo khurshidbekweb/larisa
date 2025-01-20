a533d79e76c04c94a4ffc6c54b3c8b731778cbab
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-01-20 11:53:08 +0500

    resolve problem started

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
     const {t} = useTranslation()
     return (
         <footer className="bg-[#1A5C7F]"> 
-            <div className="footer-inner max-w-7xl xl:py-20 md:py-10 py-5 mx-auto flex flex-col xl:flex-row xl:justify-between">
+            <div className="footer-inner max-w-7xl xl:py-20 md:py-10 py-5 mx-auto flex flex-col xl:flex-row xl:justify-between xl:items-start">
                 <div className="flex flex-col md:flex-row md:justify-between xl:flex-col xl:items-start xl:w-[30%]">
                     <img src={logo} alt="footer logo" />
                     <ul className="flex items-center gap-x-3 mt-5">
@@ -27,7 +27,7 @@ const Footer = () => {
                         </li>
                     </ul>
                 </div>
-                <ul className="text-white mt-8 flex flex-col md:flex-row space-y-4 md:space-y-0 md:justify-between xl:flex-1">
+                <ul className="text-white mt-8 flex flex-col md:flex-row space-y-4 md:space-y-0 md:justify-between xl:flex-1 xl:mt-0">
                     <li className='flex flex-col items-start'>
                         <h2 className='text-[20px] md:text-[20px] xl:text-[24px] font-bold'>{t('services')}</h2>
                         <a className='text-[14px] md:text-[16px] xl:text-[18px] mt-3' href="#">{t('service_card1')}</a>
