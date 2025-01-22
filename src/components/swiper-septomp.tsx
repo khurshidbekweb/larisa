be579ef1b112952b46737c024f52ed218205e085
tarpeeton <tarpeetonteam@gmail.com>
2025-01-22 10:52:41 +0500

    #slider data  tranlate

--- src/components/swiper-septomp.tsx
+++ src/components/swiper-septomp.tsx
@@ -1,35 +1,19 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Keyboard, Pagination } from "swiper/modules";
 import SwiperCore from "swiper";
 import classNames from "classnames";
-import user1 from "@/assets/image/user1.jpg";
-import user2 from "@/assets/image/user2.jpg";
-import user3 from "@/assets/image/user3.jpg";
-import user4 from "@/assets/image/user4.jpg";
 import prev from "@/assets/image/prev.png";
 import next from "@/assets/image/next.png";
-// @ts-expect-error
 import "swiper/css";
-// @ts-expect-error
 import "swiper/css/pagination";
+import { SliderData } from '@/constants/semtoms';
+import { SlideItemProps } from '@/lib/types/slider';
+import { useTranslation } from 'react-i18next';
 
-interface Slide {
-  id: number;
-  image: string;
-  title: string;
-}
-
-const SwiperCarousel: React.FC = () => {
-  const slides: Slide[] = [
-    { id: 1, image: user1, title: "Усталость" },
-    { id: 2, image: user2, title: "Скачки веса" },
-    { id: 3, image: user3, title: "Выпадение волос" },
-    { id: 4, image: user4, title: "Сухость кожи" },
-    { id: 5, image: user1, title: "Сухость кожи" },
-    { id: 6, image: user2, title: "Сухость кожи" },
-  ];
+const SwiperCarousel = () => {
+  const { i18n } = useTranslation();
+  const lng: 'ru' | 'uz' = i18n.language as 'ru' | 'uz';
 
   const swiperRef = useRef<SwiperCore | null>(null);
   const [activeIndex, setActiveIndex] = useState(0);
@@ -60,11 +44,11 @@ const SwiperCarousel: React.FC = () => {
         }}
         className="relative h-[350px] md:h-[420px]"
       >
-        {slides.map((slide, index) => (
+        {SliderData.map((slide, index) => (
           <SwiperSlide key={slide.id}>
             <SlideItem
               image={slide.image}
-              title={slide.title}
+              title={slide.title[lng]}
               isActive={index === activeIndex}
             />
           </SwiperSlide>
@@ -88,11 +72,7 @@ const SwiperCarousel: React.FC = () => {
   );
 };
 
-interface SlideItemProps {
-  image: string;
-  title: string;
-  isActive: boolean;
-}
+
 
 const SlideItem: React.FC<SlideItemProps> = ({ image, title, isActive }) => {
   return (
@@ -108,6 +88,7 @@ const SlideItem: React.FC<SlideItemProps> = ({ image, title, isActive }) => {
     >
       <img
         src={image}
+        loading='lazy'
         alt={title}
         className="w-full h-full object-cover rounded-[20px]"
       />
---
+++ src/constants/semtoms.ts
@@ -0,0 +1,21 @@
+import user1 from "@/assets/image/user1.jpg";
+import user2 from "@/assets/image/user2.jpg";
+import user3 from "@/assets/image/user3.jpg";
+import user4 from "@/assets/image/user4.jpg";
+import user5 from "@/assets/image/user5.jpg";
+
+
+
+
+
+
+
+
+export const SliderData = [
+  { id: 1, image: user1, title: { ru: "Слабость", uz: "Zaiflik" } },
+  { id: 2, image: user2, title: { ru: "Скачки веса", uz: "Vazn o'zgarishi" } },
+  { id: 3, image: user3, title: { ru: "Выпадение волос", uz: "Soch to'kilishi" } },
+  { id: 4, image: user4, title: { ru: "Сухость кожи", uz: "Teri quruqligi" } },
+  { id: 5, image: user1, title: { ru: "Чувство жажды", uz: "Chanqoqlik" } },
+  { id: 6, image: user5, title: { ru: "Раздражительность", uz: "Jahldorlik" } },
+];
