085a5deada90b79d3433caab2a9692caebb78a37
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-01-22 11:13:11 +0500

    recommunded secrion update

--- src/sections/recommunded.tsx
+++ src/sections/recommunded.tsx
@@ -1,17 +1,21 @@
 import { useTranslation } from "react-i18next";
 import Image from '@/assets/image/dieta.png'
-import homle from '@/assets/image/homla.svg'
+import buqoq from '@/assets/image/buqoq.png'
 import analez from '@/assets/image/two.png'
 import semizlik from '@/assets/image/semiz.png'
 import soglom from '@/assets/image/soglom.png'
+import suyak from '@/assets/image/suyak.png'
+import oqsil from '@/assets/image/oqsil.png'
+import bakterya from '@/assets/image/bakterya.png'
+import bel_ogriq from '@/assets/image/bel_ogrigi.png'
 
 
 const Recommunded = () => {
     const {t} = useTranslation()
     const diateInfo = [
         {
-            image: homle,
-            title: t('homla')
+            image: buqoq,
+            title: t('buqoq')
         },
         {
             image: analez,
@@ -25,24 +29,42 @@ const Recommunded = () => {
             image: soglom   ,
             title: t('soglom')
         },
+        {
+            image: suyak   ,
+            title: t('suyak_mort')
+        },
+        {
+            image:  oqsil  ,
+            title: t('suyak_oqsil')
+        },
+        {
+            image: bakterya   ,
+            title: t('bakterya')
+        },
+        {
+            image: bel_ogriq   ,
+            title: t('bel_ogrigi')
+        },
     ]
     return (
-        <section className="max-w-7xl mx-auto relative container mt-10 md:mt-14 xl:mt-20">
+        <section className=" relative mt-10 md:mt-14 xl:mt-20">
             <div className="recommunded-inner ">
                 <h2 className="text-[#C9A690] font-bold text-[14px] md:text-[16px] text-center">{t('whom_')}</h2>
                 <h4 className="text-[24px] font-semibold md:text-[32px] xl:text-[40px] text-center mt-5 md:mt-8 px-4">{t('whom_me')}</h4>
-            </div>
-            <div className="flex flex-col xl:flex-row space-y-4 md:space-y-6 xl:space-y-0 justify-between items-center mt-5 md:mt-8 xl:mt-10">
-                <img className="xl:w-[519px] px-4 bg-white" src={Image} alt="" />
-                <ul className="flex flex-col md:space-y-0 bg-[#89BFDB1A] w-full md:flex-row md:gap-5 md:flex-wrap space-y-3 px-4 md:p-4 xl:py-8 xl:p-6">
-                    {diateInfo.map((el, i) => (
-                        <li key={i} className="flex items-center gap-x-3 max-w-[328px] md:max-w-[322px] xl:max-w-[320px]">
-                            <img src={el.image} alt="image" />
-                            <h4 className="text-[14px] md:text-[16px] xl:text-[18px]">{el.title}</h4>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                <div className="bg-[#89BFDB1A] py-3 md:py-0 md:mt-32 xl:mt-20">
+                    <div className="flex max-w-7xl mx-auto flex-col xl:flex-row space-y-4 md:space-y-6 xl:space-y-0 justify-between items-center mt-5 md:mt-8 xl:mt-10">
+                        <img className="xl:w-[550px] px-4 md:-mt-16 xl:-mt-5" src={Image} alt="" />
+                        <ul className="flex flex-col md:space-y-0 w-full md:flex-row md:gap-5 md:flex-wrap space-y-3 px-4 md:p-4 xl:py-8 xl:p-6">
+                            {diateInfo.map((el, i) => (
+                                <li key={i} className="flex items-center gap-x-3 w-[328px] md:w-[322px] xl:w-[320px]">
+                                    <img src={el.image} alt="image" />
+                                    <h4 className="text-[14px] md:text-[16px] xl:text-[18px]">{el.title}</h4>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>            
         </section>
     );
 };
