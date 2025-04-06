046bd817116c562ee9ed264ea36fa848d2cffe44
tarpeeton <tarpeetonteam@gmail.com>
2025-04-06 12:51:51 +0300

    GoogleTag manager

--- src/App.tsx
+++ src/App.tsx
@@ -95,6 +95,23 @@ const App = () => {
     }
   }, []);
 
+useEffect(() => {
+    const gtmScript = document.createElement("script");
+    gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N58S62CZ');`;
+    document.head.appendChild(gtmScript);
+
+    const gtmNoscript = document.createElement("noscript");
+    gtmNoscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N58S62CZ" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+    document.body.appendChild(gtmNoscript);
+
+    return () => {
+      // Cleanup
+      document.head.removeChild(gtmScript);
+      document.body.removeChild(gtmNoscript);
+    };
+  }, []);
+
+
   const currentLang: LangKeys = (i18n.language as LangKeys) || "uz"; // Fallback to "uz" if language is not set
   const currentSEO = seoText[currentLang] || seoText.uz;
 
